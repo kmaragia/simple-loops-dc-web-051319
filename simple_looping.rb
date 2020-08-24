5faa6b37264970269e0fdfede407ee714445fcde
Kemunto Maragia <kmaragia@email.wm.edu>
2020-08-24 21:10:27 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,1 +1,22 @@
+def loop_iterator
+  count = 0
+  loop do 
+    puts "Welcome to Flatiron School's Web Development Course!"
+    count += 1 
+    break if count == 7
+  end
+end 
 
+def times_iterator
+  7.times do 
+    puts"
+end
+
+def while_iterator
+end
+
+def until_iterator
+end
+
+def for_iterator
+end
