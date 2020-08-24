939f36fd7c5e91e82ff9823b5cad064f562b9880
Kemunto Maragia <kmaragia@email.wm.edu>
2020-08-24 21:25:26 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,12 +16,20 @@ end
 def while_iterator
   count = 0 
   while count < 7
-    
-end
+    puts "Welcome to Flatiron School's Web Development Course!"
+    count += 1
+  end
 end
 
 def until_iterator
+  count = 0 
+  until count == 7
+   puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
 def for_iterator
+  for person in 1..7
+   puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
