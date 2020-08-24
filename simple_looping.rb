e67beaf34169bb189aca6490f9ab3ffb9ff04665
Kemunto Maragia <kmaragia@email.wm.edu>
2020-08-24 21:27:33 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,35 +1,35 @@
-def loop_iterator
+def loop_iterator (num)
   count = 0
   loop do 
     puts "Welcome to Flatiron School's Web Development Course!"
     count += 1 
-    break if count == 7
+    break if count == num
   end
 end 
 
-def times_iterator
-  7.times do 
+def times_iterator(num)
+  num.times do 
     puts"Welcome to Flatiron School's Web Development Course!"
   end 
 end
 
-def while_iterator
+def while_iterator(num)
   count = 0 
-  while count < 7
+  while count < num
     puts "Welcome to Flatiron School's Web Development Course!"
     count += 1
   end
 end
 
-def until_iterator
+def until_iterator (num)
   count = 0 
-  until count == 7
+  until count == num
    puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
 
-def for_iterator
-  for person in 1..7
+def for_iterator(num)
+  for person in 1..num
    puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
